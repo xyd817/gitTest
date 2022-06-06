a904418a6feadf1a034916abac682d3dbb53d9a2
xyd <xyd@123.com>
2022-06-06 16:16:04 +0800

    git test3

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -5,5 +5,7 @@
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello Git");
+        System.out.println("hello Java");
+        System.out.println("hello Scala");
     }
 }
